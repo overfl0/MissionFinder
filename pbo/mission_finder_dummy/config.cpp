95730a3aa9c307d2240c6a289cf41fc87acf079d
Lukasz Taczuk <overfl0@users.noreply.github.com>
2019-11-02 23:16:10 +0100

    Make the module discoverable by Pythia

---
+++ pbo/mission_finder_dummy/config.cpp
@@ -0,0 +1,8 @@
+class CfgPatches {
+    class MissionFinder_Sample_Patches {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 1.66;
+        requiredAddons[] = {};
+    };
+};
